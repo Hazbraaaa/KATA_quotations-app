b9e3ca12ceca7b149a2ca6952339553eb35dd936
chazera <hazera.c@gmail.com>
2025-02-28 12:29:43 +0100

    etape 6 : convaincu que le principe du form n'est pas ce qui était attendu

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // init variables
-const submitButton = document.querySelector('#submit-button');
+const form = document.querySelector('#form');
 const quote = document.querySelector('#quote');
 const personQuoted = document.querySelector('#person-quoted');
-const quoteList = document.querySelector('#quote-list')
+const quoteList = document.querySelector('#quote-list');
 let text = "";
 let author = "";
+let quoteCount = 1; // already one quote of Simone de Beauvoir
 
 
 // init functions
@@ -17,19 +18,23 @@ function addQuote(text, author) {
     const quoteBlock = document.createElement('p');
     const authorBlock = document.createElement('p');
     const quoteDiv = document.createElement('div');
+    const count = document.querySelector('#count');
 
     quoteBlock.classList.add('text');
-    quoteBlock.innerText = `Citation : ${text}`;
+    quoteBlock.innerText = `Citation : "${text}"`;
     authorBlock.classList.add('auhtor');
     authorBlock.innerText = `Auteur/Autrice : ${author}`;
     quoteDiv.classList.add('quote');
     quoteDiv.appendChild(quoteBlock);
     quoteDiv.appendChild(authorBlock);
     quoteList.appendChild(quoteDiv);
+    quoteCount++;
+    count.innerText = `${quoteCount} citations`;
 }
 
+
 // execute code
-submitButton.addEventListener('click', () => {
+form.addEventListener('submit', () => {
     getQuotation();
     addQuote(text, author);
 })
