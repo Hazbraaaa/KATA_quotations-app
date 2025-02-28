e466a3b7c5c5af510119e54e02cd52be40564978
chazera <hazera.c@gmail.com>
2025-02-28 11:28:10 +0100

    etape 5 : souci avec la citation qui reste pas, j'avance quand même

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const submitButton = document.querySelector('#submit-button');
 const quote = document.querySelector('#quote');
 const personQuoted = document.querySelector('#person-quoted');
+const quoteList = document.querySelector('#quote-list')
 let text = "";
 let author = "";
 
@@ -10,12 +11,25 @@ let author = "";
 function getQuotation() {
     text = quote.value;
     author = personQuoted.value;
-    console.log(text);
-    console.log(author);
 }
 
+function addQuote(text, author) {
+    const quoteBlock = document.createElement('p');
+    const authorBlock = document.createElement('p');
+    const quoteDiv = document.createElement('div');
+
+    quoteBlock.classList.add('text');
+    quoteBlock.innerText = `Citation : ${text}`;
+    authorBlock.classList.add('auhtor');
+    authorBlock.innerText = `Auteur/Autrice : ${author}`;
+    quoteDiv.classList.add('quote');
+    quoteDiv.appendChild(quoteBlock);
+    quoteDiv.appendChild(authorBlock);
+    quoteList.appendChild(quoteDiv);
+}
 
 // execute code
 submitButton.addEventListener('click', () => {
     getQuotation();
+    addQuote(text, author);
 })
